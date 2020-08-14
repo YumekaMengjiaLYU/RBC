96931da082e1675f4c57009521eea051927634e2
Tinashe Tapera <tinashemtapera@gmail.com>
2020-08-14 18:28:56 -0400

    reuploading missing

--- PennLINC/multiproc_pnc.sh
+++ PennLINC/multiproc_pnc.sh
@@ -1,2 +1,2 @@
 #echo "subject,download_ok,check_ok,date" > /storage/ttapera/RBC/data/completed.txt
-seq 1 1601 | xargs -P 10 -n 1 /storage/ttapera/RBC/PennLINC/process_subject.sh
+seq 1 885 | xargs -P 10 -n 1 /storage/ttapera/RBC/PennLINC/process_subject.sh
