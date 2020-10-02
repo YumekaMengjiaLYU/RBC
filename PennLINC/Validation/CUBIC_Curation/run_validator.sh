c8acc5362b418f483aa0b0d3984f101328d51008
TinasheMTapera <tinashemtapera@gmail.com>
2020-10-02 17:08:34 -0400

    progress on cubic
    - PNC ready with DWI and ASL included in fieldmaps
    - HBN in progress (@zizu)
    - NKI waiting on ASL assignment

--- PennLINC/Validation/CUBIC_Curation/run_validator.sh
+++ PennLINC/Validation/CUBIC_Curation/run_validator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-bids-validator --json --verbose /cbica/projects/RBC/flywheel_curation/RBC/data/bids_datasets/RBC_PNC > pnc_issues.json &
+time bids-validator --json --verbose /cbica/projects/RBC/RBC_RAWDATA/bidsdatasets/PNC > pnc_issues.json &
 
-bids-validator --json --verbose /cbica/projects/RBC/flywheel_curation/RBC/data/bids_datasets/RBC_NKI > nki_issues.json &
+time bids-validator --json --verbose /cbica/projects/RBC/RBC_RAWDATA/bidsdatasets/NKI > nki_issues.json &
 
-bids-validator --json --verbose /cbica/projects/RBC/flywheel_curation/RBC/data/bids_datasets/RBC_HBN > hbn_issues.json &
+time bids-validator --json --verbose /cbica/projects/RBC/RBC_RAWDATA/bidsdatasets/HBN > hbn_issues.json &
