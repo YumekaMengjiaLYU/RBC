ddeed5494eaadf136e89b7da060378946789d8cc
TinasheMTapera <tinashemtapera@gmail.com>
2020-10-13 15:02:39 -0400

    testing validator

--- PennLINC/Validation/CUBIC_Curation/run_validator.sh
+++ PennLINC/Validation/CUBIC_Curation/run_validator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-time bids-validator --json --verbose /cbica/projects/RBC/RBC_RAWDATA/bidsdatasets/PNC > pnc_issues.json &
+time bids-validator --json --verbose --ignoreSessionConsistency /cbica/projects/RBC/RBC_RAWDATA/bidsdatasets/PNC > pnc_issues.json &
 
-time bids-validator --json --verbose /cbica/projects/RBC/RBC_RAWDATA/bidsdatasets/NKI > nki_issues.json &
+time bids-validator --json --verbose --ignoreSessionConsistency /cbica/projects/RBC/RBC_RAWDATA/bidsdatasets/NKI > nki_issues.json &
 
-time bids-validator --json --verbose /cbica/projects/RBC/RBC_RAWDATA/bidsdatasets/HBN > hbn_issues.json &
+time bids-validator --json --verbose --ignoreSessionConsistency /cbica/projects/RBC/RBC_RAWDATA/bidsdatasets/HBN > hbn_issues.json &
