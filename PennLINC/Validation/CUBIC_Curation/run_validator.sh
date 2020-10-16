fee24be51182cfa6b5655c7042933a0882a3b8b8
TinasheMTapera <tinashemtapera@gmail.com>
2020-10-16 07:24:56 -0400

    validator works now in series + read mode only

--- PennLINC/Validation/CUBIC_Curation/run_validator.sh
+++ PennLINC/Validation/CUBIC_Curation/run_validator.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 
-# change permissions to write only
-find /cbica/projects/RBC/RBC_RAWDATA/bidsdatasets/PNC -type f -iname "*" -print0 | xargs -I {} -0 chmod 444 {}
-
-find /cbica/projects/RBC/RBC_RAWDATA/bidsdatasets/NKI -type f -iname "*" -print0 | xargs -I {} -0 chmod 444 {}
-
-find /cbica/projects/RBC/RBC_RAWDATA/bidsdatasets/HBN -type f -iname "*" -print0 | xargs -I {} -0 chmod 444 {}
-
 # run validator
 time bids-validator --json --verbose --ignoreSessionConsistency /cbica/projects/RBC/RBC_RAWDATA/bidsdatasets/PNC > pnc_issues.json
 
 time bids-validator --json --verbose --ignoreSessionConsistency /cbica/projects/RBC/RBC_RAWDATA/bidsdatasets/NKI > nki_issues.json
 
 time bids-validator --json --verbose --ignoreSessionConsistency /cbica/projects/RBC/RBC_RAWDATA/bidsdatasets/HBN > hbn_issues.json
-
-# revert permissions
-find /cbica/projects/RBC/RBC_RAWDATA/bidsdatasets/NKI -type f -iname "*" -print0 | xargs -I {} -0 chmod 644 {}
-
-find /cbica/projects/RBC/RBC_RAWDATA/bidsdatasets/PNC -type f -iname "*" -print0 | xargs -I {} -0 chmod 644 {}
-
-find /cbica/projects/RBC/RBC_RAWDATA/bidsdatasets/HBN -type f -iname "*" -print0 | xargs -I {} -0 chmod 644 {}
