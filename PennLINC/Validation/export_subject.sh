dbe1a994b1209f33e6f34affe00631c70df20937
Sydney Covitz <sydney.covitz@gmail.com>
2021-03-10 12:08:17 -0500

    curation notebooks and flywheel export

--- PennLINC/Validation/export_subject.sh
+++ PennLINC/Validation/export_subject.sh
@@ -3,4 +3,4 @@
 project=$1
 subject=$2
 
-fw export bids --project $project --subject $subject $HOME/flywheel_curation/RBC/data/bids_datasets/$project
+fw export bids --project $project --subject $subject $HOME/RBC_RAWDATA/bidsdatasets/$project
