46bf05c470c3d328ae3a95935205d6919b923783
Sydney Covitz <sydney.covitz@gmail.com>
2021-07-28 12:56:11 -0400

    droping unused col

--- PennLINC/Generic/concatenator.py
+++ PennLINC/Generic/concatenator.py
@@ -10,15 +10,16 @@ import sys
 
 csv_dir = sys.argv[1]
 
-columns = ["SubjectID"] # "HasOutput", "HasHTML", "NoErrorsToReport", "HasFuncDir", "HasBold", "ProducedFuncDir", "RanSurfBold", "RanVolBold", "HasErrorFile", "RuntimeErrorDescription", "OSErrorDescription", "CommandErrorDescription", "HadScratchSpace", "HadRAMSpace", "HadDiskSpace", "FinishedSuccessfully"]
+#columns = ["SubjectID"] # "HasOutput", "HasHTML", "NoErrorsToReport", "HasFuncDir", "HasBold", "ProducedFuncDir", "RanSurfBold", "RanVolBold", "HasErrorFile", "RuntimeErrorDescription", "OSErrorDescription", "CommandErrorDescription", "HadScratchSpace", "HadRAMSpace", "HadDiskSpace", "FinishedSuccessfully"]
 
-df = pd.DataFrame(np.nan, index=range(0,1), columns=columns, dtype="string")
+df = pd.DataFrame(np.nan, index=range(0,1), dtype="string")
 
 for csv_path in Path(csv_dir).rglob('*.csv'):
     #subprocess.run(['datalad', 'get', csv_path])
     sub_df = pd.read_csv(str(csv_path))
     df = pd.concat([df, sub_df])
 
+df.drop['Unnamed: 0']
 df.to_csv(sys.argv[2], index=False)
                     
 # THEN RUN THIS THROUGH THE SUMMARY REPORT SCRIPTS! 
