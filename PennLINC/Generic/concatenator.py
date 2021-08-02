170af94af27c2eec54b79316ccd4214a72dada62
Sydney Covitz <sydney.covitz@gmail.com>
2021-08-02 07:13:51 -0400

    concat

--- PennLINC/Generic/concatenator.py
+++ PennLINC/Generic/concatenator.py
@@ -10,15 +10,20 @@ import sys
 
 csv_dir = sys.argv[1]
 
-columns = ["SubjectID",  "HasOutput", "HasHTML", "NoErrorsToReport", "HasFuncDir", "HasBold", "ProducedFuncDir", "RanSurfBold", "RanVolBold", "HasErrorFile", "RuntimeErrorDescription", "OSErrorDescription", "CommandErrorDescription", "HadScratchSpace", "HadRAMSpace", "HadDiskSpace", "FinishedSuccessfully"]
+#columns = ["SubjectID",  "HasOutput", "HasHTML", "NoErrorsToReport", "HasFuncDir", "HasBold", "ProducedFuncDir", "RanSurfBold", "RanVolBold", "HasErrorFile", "RuntimeErrorDescription", "OSErrorDescription", "CommandErrorDescription", "HadScratchSpace", "HadRAMSpace", "HadDiskSpace", "FinishedSuccessfully"]
 
-df = pd.DataFrame(np.nan, index=range(0,1), columns=columns, dtype="string")
+#df = pd.DataFrame(np.nan, index=range(0,1), columns=columns, dtype="string")
 
+#concatenated_dataframes = pd.concat([dataframe1, dataframe2], axis=1)
+cntr = 0
 for csv_path in Path(csv_dir).rglob('*.csv'):
     #subprocess.run(['datalad', 'get', csv_path])
     sub_df = pd.read_csv(str(csv_path))
-    df = pd.concat([df, sub_df])
-
+    if cntr == 0: 
+        df = sub_df
+    else:
+        df = pd.concat([df, sub_df], axis=1)
+    cntr += 1
 #final = df.drop('Unnamed: 0', axis=1)
 df.to_csv(sys.argv[2], index=False)
                     
