64790dfc547409c918898f5acacb3e2328a040aa
Sydney Covitz <sydney.covitz@gmail.com>
2021-08-06 11:23:10 -0400

    for loop

--- PennLINC/Generic/concatenator.py
+++ PennLINC/Generic/concatenator.py
@@ -18,14 +18,26 @@ csv_dir = sys.argv[1]
 cntr = 0
 for csv_path in Path(csv_dir).rglob('*.csv'):
     #subprocess.run(['datalad', 'get', csv_path])
-    sub_df = pd.read_csv(str(csv_path))
-    #columns = list(sub_df.columns())
-    if cntr == 0: 
-        df = sub_df
-    else:
-        df = pd.concat([df, sub_df])
     cntr += 1
+    sub_df = pd.read_csv(str(csv_path))    
+    columns = list(sub_df.columns())
+    if cntr > 0:
+        break 
+
+df = pd.DataFrame(np.nan, index=range(0,1), columns=columns, dtype="string")
+
+
+for csv_path in Path(csv_dir).rglob('*.csv'):
+    sub_df = pd.read_csv(str(csv_path))
+    df = pd.concat([df, sub_df])
+
+#    if cntr == 0: 
+#        df = pd.concat([df, sub_df])
+#    else:
+#        df = pd.concat([df, sub_df])
+#    cntr += 1
 #final = df.drop('Unnamed: 0', axis=1)
+
 df.to_csv(sys.argv[2], index=False)
                     
 # THEN RUN THIS THROUGH THE SUMMARY REPORT SCRIPTS! 
