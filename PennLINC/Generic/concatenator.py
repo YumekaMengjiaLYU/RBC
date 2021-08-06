abad3165e5100eb8a957f5503ee2a7c2b36afecb
Sydney Covitz <sydney.covitz@gmail.com>
2021-08-06 11:39:35 -0400

    more prints

--- PennLINC/Generic/concatenator.py
+++ PennLINC/Generic/concatenator.py
@@ -37,7 +37,8 @@ for csv_path in Path(csv_dir).rglob('*.csv'):
 #        df = pd.concat([df, sub_df])
 #    cntr += 1
 #final = df.drop('Unnamed: 0', axis=1)
-
+print("OUTPUT FILE", sys.argv[2])
+print("OUTPUT", df.columns)
 df.to_csv(sys.argv[2], index=False)
                     
 # THEN RUN THIS THROUGH THE SUMMARY REPORT SCRIPTS! 
